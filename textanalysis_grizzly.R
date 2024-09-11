1ffb2318bd6249df4324aa9726c9d145c725a1d6
Janna Willoughby <jwilloughby@auburn.edu>
2024-09-11 17:27:20 -0500

    update to analysis code looping

--- textanalysis_grizzly.R
+++ textanalysis_grizzly.R
@@ -5,7 +5,7 @@ library(Rtsne)
 library(rsvd) 
 library(geometry)
 
-setwd("/Users/jannawilloughby/Google Drive/My Drive/Willoughby lab/projects - active/grizzly_text/grizzly_textanalysis/")
+#setwd("/Users/jannawilloughby/Google Drive/My Drive/Willoughby lab/projects - active/grizzly_text/grizzly_textanalysis/")
 setwd("/Users/jrw0107/Google Drive/My Drive/Willoughby lab/projects - active/grizzly_text/grizzly_textanalysis/")
 
 ####clean and set up data####
@@ -20,6 +20,7 @@ setwd("/Users/jrw0107/Google Drive/My Drive/Willoughby lab/projects - active/gri
 #correct data formats, etc
 fulldata = read.table("Wyoming Grizzly bear documentsV3.csv", sep=",", header=T, comment.char = "")
 data = fulldata[fulldata$type!="NOTRELEVANT",]
+#data = fulldata[fulldata$type!="NOTRELEVANT" & fulldata$Condensed_gov_actors!="State Judicial" & fulldata$Condensed_gov_actors!="State Legislature" ,]
 monthdays = data.frame(month=1:12, days=c(31,28,31,30,31,30,31,31,30,31,30,31), cdays=c(0,31,59,90,120,151,181,212,243,273,304,334))
 
 for(r in 1:nrow(data)){
@@ -35,21 +36,23 @@ for(r in 1:nrow(data)){
   data$Year[r]  = tt
   data$Month[r] = strsplit(data$Pub_date[r], split="/")[[1]][1]
   data$Day[r]   = strsplit(data$Pub_date[r], split="/")[[1]][2]
-  data$Seqdate[r] = ((as.numeric(data$Year[r])-1)*365) + monthdays$cdays[monthdays$month==as.numeric(data$Month[r])] + as.numeric(data$Day[r])
+  month = as.character(data$Month[r])
+  if(length(strsplit(month,"")[[1]])==1){month=paste("0",month,sep="")}
+  day = data$Day[r]
+  if(length(strsplit(day,"")[[1]])==1){day=paste("0",day,sep="")}
+  data$SeqdateC[r] = paste(data$Year[r], month, day, sep="")
 }
-data$Seqdate = data$Seqdate - min(data$Seqdate, na.rm=T)
-
-
-
+data$SeqdateC = as.numeric(data$SeqdateC)
 
 #set variables
-lowerthreshold = c(5,10,20) #words that do not appear in at least this many documents will be removed
+lowerthreshold = c(10,20) #words that do not appear in at least this many documents will be removed
+#datasummary = NULL
 
-####find topics for ideas in focal columns (separately)####
-for(c in 12:24){ #these are columns of interest
+####find topics for ideas in Condensed_gov_actors columns (separately)####
+for(c in 14:26){ #these are columns of interest
   for(l in 1:length(lowerthreshold)){
     documents = data.frame(docs = data[,c])
-    metadata  = data.frame(level = data$Condensed_gov_actors)
+    metadata  = data.frame(level = data$Condensed_function) #Condensed_gov_actors Condensed_level Condensed_function
     
     #remove missing data rows
     t = cbind(documents, metadata)
@@ -59,7 +62,7 @@ for(c in 12:24){ #these are columns of interest
     metadata  = data.frame(level = t[,2])
     
     #analyze data with levels
-    text   = textProcessor(documents = documents$docs, metadata = metadata, customstopwords=c("said", "will", "'re"))
+    text   = textProcessor(documents = documents$docs, metadata = metadata, customstopwords=c("get","like","one","re","said","say","will"))
     output = prepDocuments(text$documents, text$vocab, text$meta, lower.thresh = lowerthreshold[l]) #should this be higher?
     if(length(output$vocab)<=5){next}
     output.stm = stm(documents = output$documents, vocab = output$vocab,  prevalence =~ level, K = 5, max.em.its = 10000, data = output$meta, init.type = "Spectral", seed = 2112)
@@ -71,8 +74,11 @@ for(c in 12:24){ #these are columns of interest
     #regressions and plots (output relates topic prevalence in each level)
     sink(paste("output/output_", lowerthreshold[l], "_",colnames(data)[c], ".txt", sep=""), append=F)
     pdf(file=paste("output/output_", lowerthreshold[l], "_",colnames(data)[c], ".pdf", sep=""), width=7, height=7, onefile=T)
-    
+    print(text)
+    print(table(t[,2]))
     print(summary(output.stm))
+    #datasummary = rbind(datasummary,c(colnames(data)[c],lowerthreshold[l],table(t[,2]),sum(table(t[,2])),length(text[[2]])))
+    
     for( i in 1:5){
       topic = paste(summary(output.stm)[[1]][i,], collapse=" ")
       level.eff  = estimateEffect(c(i) ~ (level)-1, output.stm, metadata = output$meta, uncertainty = "None")
@@ -85,9 +91,6 @@ for(c in 12:24){ #these are columns of interest
     dev.off()
   }
 }  
-
-
-
 
 
 ####find topic change over time in focal columns (separately)####
